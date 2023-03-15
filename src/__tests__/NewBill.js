6982e6a3078772635bf2b75a272ae786ad1efbba
Frederic.K <frederic.kreuder@gmail.com>
2023-03-15 16:07:38 +0100

    Test NewBill : testing uploaded file type

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -63,8 +63,84 @@ describe("Given I am connected as an employee", () => {
     });
   });
 
+  describe("When i upload invoice receipt file", () => {
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+    });
+    afterEach(() => {
+      jest.clearAllMocks()
+    });
+
+    describe("When i upload wrong file type", () => {
+      test("Then a error message is diplaying", async () => {
+        document.body.innerHTML = NewBillUI();
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
   
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          mockStore,
+          localStorage: window.localStorage,
+        });
+
+        const inputFile = screen.getByTestId("file");
+        const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+        inputFile.addEventListener("change", handleChangeFile);
+
+        fireEvent.change(inputFile, {
+          target: {
+            files: [new File(["receiptTest.mp4"], "receiptTest.mp4", { type: "video/mp4" })],
+          }
+        });
+  
+        expect(handleChangeFile).toHaveBeenCalled();
+        expect(inputFile.files[0].type).toBe("video/mp4");
+        
+        await waitFor(() => screen.getByTestId("message_file_type_error"));
+        const errorFileType = screen.queryByTestId("message_file_type_error");
+        expect(errorFileType.className).toBe("msg__error--filetype");
+      })
+    })
+
+    describe("When i upload allowed file type", () => {
+      test("Then uploaded file name is display", async () => {
+        const html = NewBillUI();
+        document.body.innerHTML = html;
+
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname });
+        };
+  
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          mockStore,
+          localStorage: window.localStorage,
+        });
+
+        const inputFile = screen.getByTestId("file");
+        const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e));
+        inputFile.addEventListener("change", handleChangeFile);
+
+        fireEvent.change(inputFile, {
+          target: {
+            files: [new File(["receiptTest.jpg"], "receiptTest.jpg", { type: "image/jpg" })],
+          }
+        });
+
+        expect(handleChangeFile).toHaveBeenCalled();
+        expect(inputFile.files[0].type).toBe("image/jpg");
+        expect(inputFile.files[0]).toEqual(File)
+        await waitFor(() => screen.getByTestId("message_file_type_error"));
+        const errorFileType = screen.queryByTestId("message_file_type_error");
+        expect(errorFileType.getAttribute("class")).toMatch(/hidden/);
+      })
+    })
 
 
-
-});
+  })
+})    
